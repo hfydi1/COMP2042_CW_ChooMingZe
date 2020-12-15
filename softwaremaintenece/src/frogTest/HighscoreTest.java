1dee032c06d44e586d253c859170751ce2adf10d
User <User@192.168.0.6>
2020-12-15 23:35:11 +0800

    Update test unit

--- softwaremaintenece/src/frogTest/HighscoreTest.java
+++ softwaremaintenece/src/frogTest/HighscoreTest.java
@@ -1,4 +1,4 @@
-package frogHighscore;
+package frogTest;
 
 
 
@@ -6,20 +6,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 
+
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
+
+import frogHighscore.Highscore;
 
 
 
 class HighscoreTest {
-	
+	/**
+	 * Test maximum score of highscore
+	 * @throws IOException
+	 */
 	@Test
 	public void testHighscore() throws IOException {
 		Highscore test = new Highscore();
 		test.setScore(810);
 		assertEquals(test.scorearr.get(0),800);
 	}
-
+	/**
+	 * Test present of negative value in highscorelist
+	 * @throws IOException
+	 */
 	@Test
 	public void testNegative() throws IOException {
 		Highscore test2 = new Highscore();
@@ -30,7 +39,10 @@ class HighscoreTest {
 			assertNotSame(test2.scorearr.get(i),-10);
 		}
 	}
-	
+	/**
+	 * Test array null
+	 * @throws IOException
+	 */
 	@Test
 	public void testNull() throws IOException {
 		Highscore test3 = new Highscore();
@@ -39,5 +51,19 @@ class HighscoreTest {
 			assertNotNull(test3.scorearr.get(0));
 		
 	}
+	
+	/**
+	 * Test sorting in array
+	 * @throws IOException
+	 */
+	@Test
+	public void testSort() throws IOException {
+		Highscore test4 = new Highscore();
+		test4.setScore(10);
+		test4.setScore(1);
+		assertTrue(test4.scorearr.get(0)>=test4.scorearr.get(1));
+	}
+	
+	
 	
 }
